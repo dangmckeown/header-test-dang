03c1791fcc81effbdb195fcb1a051a8063973ca0
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-08-03 16:28:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 for ($i = 600;$i<=610;$i++){
-
-function get_contents() {
+/*
+function get_contents() { */
   file_get_contents("https://sparks.org.uk/?p=" . $i);
-#  var_dump($http_response_header);
-}
+ var_dump($http_response_header);
+/*}
 get_contents();
 var_dump($http_response_header);
-  
+  */
 }
