d3c3b867bb8d2f3afab57d376dae050612e347b7
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-08-03 16:12:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,18 @@
 
 function get_top($url){
   file_get_contents($url);
-  var_dump($http_response_header);
-}
-
-
-
 if(! (strpos("404", $http_response_header[0]) ){
 echo $url . "<br />";
 }
+   else {
+   	echo "Page $i not found <br />";
+   }
 
 }
    
-   for ($i = 1; $i <= 2000; $i++){
+   for ($i = 1; $i <= 50; $i++){
    
 	   get_top("https://www.sparks.org.uk/?p=".$i);
 	   
    }
+   
