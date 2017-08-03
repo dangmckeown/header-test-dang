e0940f9089fc542c0a102d221cc80ef9a7c94df8
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-08-03 16:33:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ for ($i = 600;$i<=610;$i++){
 /*
 function get_contents() { */
   file_get_contents("https://sparks.org.uk/?p=" . $i);
-  if (! strpos("404",$http_response_header[0]){
+  if (! strpos("404",$http_response_header[0])){
  echo $url . " - " .$http_response_header[0];
     
   }
